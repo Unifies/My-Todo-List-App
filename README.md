d116253293148e670ca9c408c73440cc076d7f5f
Unifies <ebubenwawisdom@gmail.com>
2024-12-05 16:26:56 +0100

    My Todo List App

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# My-Todo-List-App
+![My Todo List App](<My Todo App-1.png>)
+
+Live Solution: https://unifies.github.io/My-Todo-List-App
---
+++ src/CSSinReact/OutlineComponent.jsx
@@ -0,0 +1,9 @@
+import styles from "./outline.module.css";
+
+export default function OutlineComponent() {
+  return (
+    <div>
+      <h1 className={styles.header}>Outline Component</h1>
+    </div>
+  );
+}
